8080a14468d23a63d7f359eb57022efb7b887b0a
Prajwal-Deotare <prajwaldeotare673@gmail.com>
2025-07-24 17:45:47 +0000

    operations

--- Basics/03_ConversionOperation.js
+++ Basics/03_ConversionOperation.js
@@ -1,40 +1,68 @@
-let score = "33abc"   // this is not a number(NaN)
-console.log(typeof(score))
-console.log(typeof score)
+// let score = "33abc"   // this is not a number(NaN)
+// console.log(typeof(score))
+// console.log(typeof score)
 
-let convert=Number(score)
-console.log(convert)
+// let convert=Number(score)
+// console.log(convert)
 
-let age = true // true is converted to 1 and false to 0
-console.log(typeof(age))
-let ageConvert = Number(age)
-console.log(ageConvert)
-console.log(typeof(ageConvert))
+// let age = true // true is converted to 1 and false to 0
+// console.log(typeof(age))
+// let ageConvert = Number(age)
+// console.log(ageConvert)
+// console.log(typeof(ageConvert))
 
-let gate= null // null is converted to 0
-console.log(typeof(gate))
-let gateConvert = Number(gate)
-console.log(gateConvert)
-console.log(typeof(gateConvert))
+// let gate= null // null is converted to 0
+// console.log(typeof(gate))
+// let gateConvert = Number(gate)
+// console.log(gateConvert)
+// console.log(typeof(gateConvert))
 
-let name = undefined // undefined is converted to NaN
-console.log(typeof(name))
-let nameConvert = Number(name)
-console.log(nameConvert)
-console.log(typeof(nameConvert))
+// let name = undefined // undefined is converted to NaN
+// console.log(typeof(name))
+// let nameConvert = Number(name)
+// console.log(nameConvert)
+// console.log(typeof(nameConvert))
 
-let bool = "string"
-let boolConvert = Boolean(bool) 
-console.log(boolConvert)
-console.log(typeof(boolConvert))
+// let bool = "string"
+// let boolConvert = Boolean(bool) 
+// console.log(boolConvert)
+// console.log(typeof(boolConvert))
 
-let string = "Hello World" 
-let stringConvert = String(string)
-console.log(stringConvert)
-console.log(typeof(stringConvert))
+// let string = "Hello World" 
+// let stringConvert = String(string)
+// console.log(stringConvert)
+// console.log(typeof(stringConvert))
 
 
-let strin = "Hello World" 
-let stringConver = Boolean(strin)
-console.log(stringConver)
-console.log(typeof(stringConver))
+// let strin = "Hello World" 
+// let stringConver = Boolean(strin)
+// console.log(stringConver)
+// console.log(typeof(stringConver))
+
+
+//*******************   Operations  *****************//
+
+let val = 3
+let negVal = -val
+//console.log(negVal)
+
+let str1 = "Prajwal"
+let str2 = " Deotare"
+//console.log(str1+str2)
+
+// console.log("2" + 3)
+
+// console.log(2 + "3")
+
+// console.log("2" + 3 + 4)   
+
+// console.log(2 + 3 + "4")  
+
+// console.log(+true)
+// console.log(+false)
+// console.log(+"")
+
+
+let counter = 1
+counter ++;
+console.log(counter)
