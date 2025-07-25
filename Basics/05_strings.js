d454fe45f8884dada8f8eeadc595a65a57df3a40
Prajwal-Deotare <prajwaldeotare673@gmail.com>
2025-07-25 08:31:09 +0000

    strings

---
+++ Basics/05_strings.js
@@ -0,0 +1,20 @@
+// const name = "Prajwal"; 
+// const repocount = 10;
+// console.log(name + repocount + " value")
+// console.log(`my name is ${name} and i have ${repocount} repositories`);
+
+
+const gameName = new String("        Prajwal ");
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("j"));
+
+
+const newstring = gameName.substring(0, 4);
+// console.log(newstring);
+// console.log(gameName.split(" "));
+//console.log(gameName.replace("Prajwal", "Prajwal Deoatre"));
+console.log(gameName)
+console.log(gameName.trim());
