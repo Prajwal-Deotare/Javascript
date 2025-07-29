718f21c65b095b03b6dde757477bc06c8c88a85a
Prajwal Deotare <prajwaldeotare673@gmail.com>
2025-07-29 09:20:26 +0000

    commit

---
+++ control_flow_04/one.js
@@ -0,0 +1,26 @@
+// if 
+
+if(true){
+
+}
+
+if(false){
+
+}
+
+const is_user_loggedIn = true;
+if(is_user_loggedIn){
+
+}
+
+if(2 ==="2"){   // strict equality check
+    console.log("2 ");
+}
+
+temperature = 45;
+if(temperature === 45){
+    console.log(`Temperature is ${temperature}`);
+}
+else{
+console.log("temperature is grater than 50")
+}
